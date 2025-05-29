69c269ea26aac345af1abc34bce5c0c3e4d48d34
drirodri <adriano.rsilva97@gmail.com>
2025-05-29 15:13:03 -0300

    feat: added new navbar using daisyui

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,26 @@
-import styles from "./Navbar.module.css";
 import { useTranslations } from "next-intl";
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.title}>Adriano</div>
-      <ul className="flex space-x-4">
-        <li>
-          <a href="#about" className={styles.link}>
-            {t("about")}
-          </a>
-        </li>
-        <li>
-          <a href="#projects" className={styles.link}>
-            {t("projects")}
-          </a>
-        </li>
-        <li>
-          <a href="#contact" className={styles.link}>
-            {t("contact")}
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Adriano</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="#about">{t("about")}</a>
+          </li>
+          <li>
+            <a href="#projects">{t("projects")}</a>
+          </li>
+          <li>
+            <a href="#contact">{t("contact")}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
