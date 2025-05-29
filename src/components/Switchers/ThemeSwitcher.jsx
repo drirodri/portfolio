69cebfb8ab966159a2a034321937a78828e2ac37
drirodri <adriano.rsilva97@gmail.com>
2025-05-29 18:11:18 -0300

    refactor: moved ThemeSwitcher

--- src/components/Switchers/ThemeSwitcher.jsx
+++ src/components/Switchers/ThemeSwitcher.jsx
@@ -31,7 +31,7 @@ export default function ThemeSwitcher() {
         className="hidden"
       />
       <svg
-        className="swap-off h-10 w-10 fill-current"
+        className="swap-off h-6 w-6 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
@@ -39,7 +39,7 @@ export default function ThemeSwitcher() {
       </svg>
 
       <svg
-        className="swap-on h-10 w-10 fill-current"
+        className="swap-on h-6 w-6 fill-current"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
