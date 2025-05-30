d1703e121587c7296959c8f4b17edef8e31cde4b
drirodri <adriano.rsilva97@gmail.com>
2025-05-30 13:45:54 -0300

    feat: added portfolio link to Contact component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -7,7 +7,21 @@ export default function Contact() {
   return (
     <section id="contact" className="flex flex-col items-center p-4">
       <h2 className="text-3xl font-bold mb-4">{t("title")}</h2>
-      <p className="mb-4">{t("description")}</p>
+      <p className="mb-4">
+        {t.rich("description", {
+          resume: (chunks) => (
+            <a
+              href="https://docs.google.com/document/d/1QECjqnPUg9O2m4A6IEK0RC3gSozoWjogOqkmOMvlXb8/edit?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline visted:text-blue-700"
+              aria-label="Resume"
+            >
+              {chunks}
+            </a>
+          ),
+        })}
+      </p>
       <Socials />
     </section>
   );
