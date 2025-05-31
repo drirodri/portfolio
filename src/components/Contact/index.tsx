5973a83af004ba1fa838a3d65acea15bb85edd9a
drirodri <adriano.rsilva97@gmail.com>
2025-05-30 22:04:53 -0300

    fix: removed portfolio link for now

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -4,6 +4,9 @@ import Socials from "./Socials";
 export default function Contact() {
   const t = useTranslations("Contact");
 
+  const resumeUrl =
+    "https://docs.google.com/document/d/1QECjqnPUg9O2m4A6IEK0RC3gSozoWjogOqkmOMvlXb8/edit?usp=sharing";
+
   return (
     <section id="contact" className="flex flex-col items-center p-4">
       <h2 className="text-3xl font-bold mb-4">{t("title")}</h2>
@@ -11,7 +14,7 @@ export default function Contact() {
         {t.rich("description", {
           resume: (chunks) => (
             <a
-              href="https://docs.google.com/document/d/1QECjqnPUg9O2m4A6IEK0RC3gSozoWjogOqkmOMvlXb8/edit?usp=sharing"
+              href=""
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500 hover:underline visted:text-blue-700"
