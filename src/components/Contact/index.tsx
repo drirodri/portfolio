03c071db03aa142786da129c787225274713fdf1
drirodri <adriano.rsilva97@gmail.com>
2025-05-31 12:44:27 -0300

    feat: added feature flag to resume display

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,29 +2,33 @@ import { useTranslations } from "next-intl";
 import Socials from "./Socials";
 import SectionContainer from "../SectionContainer/SectionContainer";
 
+const SHOW_RESUME = process.env.NEXT_PUBLIC_SHOW_RESUME === "true";
+
 export default function Contact() {
   const t = useTranslations("Contact");
 
-  // const resumeUrl =
-  //   "https://docs.google.com/document/d/1QECjqnPUg9O2m4A6IEK0RC3gSozoWjogOqkmOMvlXb8/edit?usp=sharing";
+  const resumeUrl =
+    "https://docs.google.com/document/d/1QECjqnPUg9O2m4A6IEK0RC3gSozoWjogOqkmOMvlXb8/edit?usp=sharing";
 
   return (
     <SectionContainer id="contact">
       <h2 className="text-3xl font-bold mb-4">{t("title")}</h2>
       <p className="mb-4">
-        {t.rich("description", {
-          resume: (chunks) => (
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline visted:text-blue-700"
-              aria-label="Resume"
-            >
-              {chunks}
-            </a>
-          ),
-        })}
+        {SHOW_RESUME
+          ? t.rich("description", {
+              resume: (chunks) => (
+                <a
+                  href={resumeUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline visted:text-blue-700"
+                  aria-label="Resume"
+                >
+                  {chunks}
+                </a>
+              ),
+            })
+          : t("descriptionNoResume")}
       </p>
       <Socials />
     </SectionContainer>
